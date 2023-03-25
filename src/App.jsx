3016d52fc84aeda44f610b47dc39110e9c3af047
Ramandeep Bhagat <rdbhagat999@gmail.com>
2023-03-25 13:06:42 +0530

    adds tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div class="container">
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
