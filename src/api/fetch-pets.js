6534c918067852f7d8a6334b912c5eaab16bcd3f
Ramandeep Bhagat <rdbhagat999@gmail.com>
2023-03-26 16:11:26 +0530

    adds search input

--- src/api/fetch-pets.js
+++ src/api/fetch-pets.js
@@ -1,2 +1,5 @@
 export const fetchPets = async () =>
   (await fetch(`http://pets-v2.dev-apis.com/pets`)).json();
+
+export const fetchPetById = async (id) =>
+  (await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`)).json();
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,46 +8,80 @@ import {
   Suspense,
 } from "solid-js";
 const PetCardComponent = lazy(() => import("../components/PetCardComponent"));
-import { fetchPets } from "../api/fetch-pets";
+import { fetchPets, fetchPetById } from "../api/fetch-pets";
 import { petStore, setPetStore } from "../stores/pet-store";
 
 const Home = () => {
+  let input;
   const [petId, setPetId] = createSignal();
 
   // runs immediately
   const [petResource, { refetch }] = createResource(fetchPets);
 
   // won't run immediately, depends upon petId. It won't run if petId = null, undefined or false
-  const [dependentResource] = createResource(petId, fetchPets);
+  const [petByIdResource] = createResource(petId, fetchPetById);
 
   let refetchInterval = 0;
 
+  const handlePetIdChange = (id) => {
+    if (id && Number(id)) {
+      setPetId(Number(id));
+    }
+  };
+
   createEffect(() => {
-    if (!petResource.loading && petResource()) {
+    if (!petResource.loading && !petResource.error && petResource()) {
       setPetStore(petResource());
     }
   });
 
   createEffect(() => {
-    if (refetchInterval <= 0) {
-      refetchInterval = setInterval(() => refetch(), 20000);
-      console.log("refetchInterval", refetchInterval);
+    if (
+      !petByIdResource.loading &&
+      !petByIdResource.error &&
+      petByIdResource()
+    ) {
+      setPetStore(petByIdResource());
     }
   });
 
+  // createEffect(() => {
+  //   if (refetchInterval <= 0) {
+  //     refetchInterval = setInterval(() => refetch(), 20000);
+  //     console.log("refetchInterval", refetchInterval);
+  //   }
+  // });
+
   onCleanup(() => {
     console.log("onCleanup");
-    return clearInterval(refetchInterval);
+    // return clearInterval(refetchInterval);
+    return;
   });
 
   return (
     <div>
       <h2 class="text-4xl">List of Awesome Pets</h2>
-      <button
-        class="px-6 py-2 mt-2 bg-green-700 text-white rounded-md"
-        onclick={() => refetch()}>
-        Refetch Pets
-      </button>
+
+      <div class="flex justify-start items-center space-x-4 mt-2">
+        <button
+          class="px-6 py-2 bg-green-700 text-white rounded-md"
+          onclick={() => {
+            input.value = null;
+            setPetId(null);
+            refetch();
+          }}>
+          Refetch Pets
+        </button>
+
+        <input
+          class="border border-gray-200 rounded-lg px-4 py-2"
+          ref={input}
+          type="number"
+          name="search"
+          placeholder="search pet by id"
+          onBlur={[handlePetIdChange, input.value]}
+        />
+      </div>
 
       <Show
         when={petStore.pets}
